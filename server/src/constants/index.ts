86ad6ee7f47c83a772eaecbdeb78b529a456fc3d
shafayet035 <shafayet035@gmail.com>
2023-10-24 01:37:38 +0600

    JWT

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -3,4 +3,4 @@ export const PORT = process.env.PORT || 8080;
 export const MONGO_DB_URI = process.env.MONGO_DB_URI;
 export const JWT_SECRET = process.env.JWT_SECRET || 'secret';
 export const AWS_EMAIL_FROM = process.env.AWS_EMAIL_FROM;
-export const JWT_EXPIRES_IN = '60s';
+export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '60s';
