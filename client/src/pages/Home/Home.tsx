34a5f4bf3888533431c601e9c064e0c16f3f763c
shafayet035 <shafayet035@gmail.com>
2023-10-29 01:12:08 +0600

    logout added

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,10 +1,26 @@
 import { Button } from '@/components/ui/button';
+import { useToast } from '@/components/ui/use-toast';
+import axios from '@/config/axios';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const { toast } = useToast();
+
+  const navigate = useNavigate();
+
+  const logoutHandler = async () => {
+    await axios.get('/auth/logout');
+    toast({
+      description: 'Logged out successfully',
+    });
+    localStorage.removeItem('access_token');
+    navigate('/login');
+  };
+
   return (
     <div className='flex flex-col justify-center items-center min-h-screen'>
       <h3 className='mb-4'> Welcome to the home page!</h3>
-      <Button>Logout</Button>
+      <Button onClick={logoutHandler}>Logout</Button>
     </div>
   );
 };
